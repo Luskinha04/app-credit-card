7ec507c26a1f262264d3a117a582177801c22651
Luskinha04 <136859524+Luskinha04@users.noreply.github.com>
2025-03-24 20:14:07 -0300

    Commit das funcionalidades já aplicadas

--- app/src/main/java/com/lucas/mycreditcard/MainActivity.kt
+++ app/src/main/java/com/lucas/mycreditcard/MainActivity.kt
@@ -1,20 +1,73 @@
 package com.lucas.mycreditcard
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val editCardNumber = findViewById<EditText>(R.id.editCardNumber)
+        val editCardExpiry = findViewById<EditText>(R.id.editCardExpiry)
+
+        // Máscara do número do cartão (1234 5678 9012 3456)
+        editCardNumber.addTextChangedListener(object : TextWatcher {
+            private var isUpdating = false
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isUpdating) return
+
+                var input = s.toString().replace(" ", "")
+                if (input.length > 16) input = input.substring(0, 16)
+
+                val formatted = StringBuilder()
+                for (i in input.indices) {
+                    formatted.append(input[i])
+                    if ((i + 1) % 4 == 0 && i != input.length - 1) {
+                        formatted.append(" ")
+                    }
+                }
+
+                isUpdating = true
+                editCardNumber.setText(formatted.toString())
+                editCardNumber.setSelection(formatted.length)
+                isUpdating = false
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        // Máscara da validade (MM/AA)
+        editCardExpiry.addTextChangedListener(object : TextWatcher {
+            private var isUpdating = false
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isUpdating) return
+
+                var input = s.toString().replace("/", "")
+                if (input.length > 4) input = input.substring(0, 4)
+
+                val formatted = StringBuilder()
+                for (i in input.indices) {
+                    if (i == 2) formatted.append("/")
+                    formatted.append(input[i])
+                }
+
+                isUpdating = true
+                editCardExpiry.setText(formatted.toString())
+                editCardExpiry.setSelection(formatted.length)
+                isUpdating = false
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 }
